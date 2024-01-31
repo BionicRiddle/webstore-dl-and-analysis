868b0f901b20a31a253eb42dfe7b0e11ce55c363
Albin Karlsson <albkar@student.chalmers.se>
2024-01-31 15:08:38 +0100

    more work on search.py

--- search.py
+++ search.py
@@ -12,12 +12,22 @@ import operator
 import re
 import threading
 import queue
+import random
+from colorama import Fore, Back, Style
 
 # Environment variables
 PRETTY_OUTPUT       = os.getenv('PRETTY_OUTPUT'     , False)
 RUN_ALL_VERSIONS    = os.getenv('RUN_ALL_VERSIONS'  , False)
 DATE_FORMAT         = os.getenv('DATE_FORMAT'       , "%Y-%m-%d_%H:%M:%S")
 NUM_THREADS         = os.getenv('NUM_THREADS'       , 1)
+
+# Create queue for threads
+thread_queue = queue.Queue()
+
+def simulate_work(extension):
+    time.sleep(random.uniform(0.1, 1))
+    print(Style.DIM + 'Analyzed extension %s' % extension)
+    print(Style.RESET_ALL, end='')
 
 # Worker Thread
 class WorkerThread(threading.Thread):
@@ -27,25 +37,31 @@ class WorkerThread(threading.Thread):
         self.queue = queue
         self.thread_id = thread_id
         self.stop_event = threading.Event()
-        self.count = 0
+        self.counter = 0
 
     def run(self):
         while not self.stop_event.is_set():
-            # Get the work from the queue, if empty, terminate
-            if self.queue.empty():
+            # Get the work from the queue, if done, terminate thread
+            try:
+                extension = self.queue.get(timeout=5)
+            except queue.Empty as e:
+                print(e)
                 break
-            extension = self.queue.get()
             #analyze(extension)
-            self.count += 1
-            print('Thread %d: %d' % (self.thread_id, self.count))
+            simulate_work(extension)
+            self.counter += 1
             self.queue.task_done()
         print('Thread %d terminated' % self.thread_id)
+
+    def get_counter(self):
+        return self.counter        
 
     def stop(self):
         self.stop_event.set()
 
 
 if __name__ == "__main__":
+
     extension = None
 
     # list of args to be removed from when parsing
@@ -80,7 +96,7 @@ if __name__ == "__main__":
                 if arg in ['-h', '--help']:
                     # I hate this
                     print('''
-Usage: python3 search.py  [options] [path_to_extensions]
+Usage: python3 search.py  [options] [path to extensions...]
 Example: python3 search.py extensions/
 
 Optional arguments:
@@ -116,59 +132,68 @@ Chalmers University of Technology, Gothenburg, Sweden
     )
 
     # Get path to extensions
-    extensions_paths = [args[-1]] if len(args) > 1 else ['extensions/']
+    extensions_paths = [args[-1]] if len(args) > 1 else ['ext/'] # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     for extensions_path in extensions_paths:
         if not os.path.isdir(extensions_path):
             print('Invalid path to extensions')
             exit(1)
 
-    # Create queue
-    queue = queue.Queue()
-
-    # Spawn threads
+    # Spawn and start threads
     threads = []
     for i in range(NUM_THREADS):
-        t = WorkerThread(queue, i)
+        t = WorkerThread(thread_queue, i)
         t.start()
         threads.append(t)
 
-    # Scan and add extensions to queue
-    extensions = []
-    for extensions_path in extensions_paths:
-        # for each dir in extensions_path
-        for dir in os.listdir(extensions_path):
-            versions = sorted([d for d in os.listdir(extensions_path + dir) if d[-4:] == ".crx"])
-            if not versions:
-                print("[+] Error (get_tmp_path) in {}: {}".format(file, 'OK') ) # TODO: Check if output format is important
-                continue
-
-            if RUN_ALL_VERSIONS:
-                for version in versions:
-                    extensions.append(extensions_path + dir + '/' + version)
-            else:
-                extensions.append(extensions_path + dir + '/' + versions[-1])
-
-    max = 10
-    i = 0
-    # Analyze extensions by sending them to threads, when done, send next extension
-    for extension in extensions:
-        if i > max:
-            break
-        i += 1
-        queue.put(extension)
-
-    # Wait for all threads to finish
-    print('Waiting for threads to finish...')
-
-    # wait for all threads to finish or if the main thread is terminated, if main thread is terminated, terminate all threads
-    try:
-        while not queue.empty():
-            pass
-    except KeyboardInterrupt:
-        print('Terminating threads...')
+    def exit(int):
+        counters = []
         for t in threads:
             t.stop()
         for t in threads:
             t.join()
+            counters.append(t.get_counter())
         print('Threads terminated')
+        print(sum(counters), 'extensions analyzed')
+        sys.exit(int)
+
+    try:
+        # Scan and add extensions to thread_queue
+        count_extensions = 0
+        extensions = []
+        for extensions_path in extensions_paths:
+            # for each dir in extensions_path
+            for dir in os.listdir(extensions_path):
+                versions = sorted([d for d in os.listdir(extensions_path + dir) if d[-4:] == ".crx"])
+                if not versions:
+                    # Empty dir
+                    print("[+] Error (get_tmp_path) in {}: {}".format(dir, 'OK') ) # TODO: Check if output format is important
+                    continue
+
+                if RUN_ALL_VERSIONS:
+                    for version in versions:
+                        thread_queue.put(extensions_path + dir + '/' + version)
+                        count_extensions += 1
+                else:
+                    thread_queue.put(extensions_path + dir + '/' + versions[-1])
+                    count_extensions += 1
+
+        print('Found %d extensions' % count_extensions)
+
+        # Wait for all threads to finish
+        print('Waiting for threads to finish...')
+
+        # wait for all threads to finish or if the main thread is terminated, if main thread is terminated, terminate all threads
+
+        thread_queue.join()
+
+        # terminate all threads
+        for t in threads:
+            t.stop()
+    except KeyboardInterrupt:
+        print()
+        print('Keyboard interrupt detected - terminating threads')
         exit(0)
+    exit(0)
+
+    
+    
