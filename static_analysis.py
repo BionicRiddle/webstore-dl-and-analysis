67c6b365effefdedeeb5a0806c107f3d66676549
OveSundberg <sam00@telia.com>
2024-02-08 10:53:55 +0100

    Began looking at statis code analysis

--- static_analysis.py
+++ static_analysis.py
@@ -1,18 +1,29 @@
+import os
+from colorama import Fore, Back, Style
+from pyunpack import Archive
 
 
-def static_analysis(extension) -> bool:
+def static_analysis(extensions) -> bool:
+
     try:
-        extracted_path = extension.get_extracted_path()
+        extracted_path = extensions.get_extracted_path()
         for files in os.listdir(extracted_path):
+
+            # Todo: Fix so we also include HTML
+            # We only care about .js files (for now)
+
             if files.endswith(".js"):
                 with open(extracted_path + files, "r") as file:
+
+                    #So here it begins
+                    print("We have arrived!")
 
 
     except Exception as e:
         print(Fore.RED + str(e) + Style.RESET_ALL)
         return False
 
-class DummyObject:
+class DummyExtensionObject:
     def __init__(self) -> None:
         self.crx_path = "DUMMY_PATH"
     def get_keyword_analysis(self) -> dict:
@@ -22,14 +33,27 @@ class DummyObject:
             "list_of_common_urls": []
         }
 
+    def get_extracted_path(self):
+        return "url"
+
 if __name__ == "__main__":
 
-    path = "path/to/file"
+    originalDirectory = "/mnt/c/Users/sam00/Desktop/MasterThesis/webstore-dl-and-analysis/" 
+    path = "/mnt/c/Users/sam00/Desktop/MasterThesis/webstore-dl-and-analysis/extensions/akoiagmmfbjephlbkncmnpdakfklipjd"
+
+    os.chdir(path)
+
+    #Filename: AKOIAGMMFBJEPHLBKNCMNPDAKFKLIPJD_5_0_0_0.crx
+
+    # Unpack files
+    Archive("AKOIAGMMFBJEPHLBKNCMNPDAKFKLIPJD_5_0_0_0.crx").extractall(originalDirectory)
+
+    os.chdir(originalDirectory)
 
     # create dummy object
-    dummy = DummyObject()
+    dummy = DummyExtensionObject()
 
-    keyword_search(dummy, path)
+    static_analysis(dummy)
 
 
     
