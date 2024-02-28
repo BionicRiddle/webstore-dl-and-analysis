88ecb3404e446a7fd78bf9115a7c8e9f0666d3bc
Albin Karlsson <albkar@student.chalmers.se>
2024-02-28 14:49:49 +0100

    pipes work

--- node/app.js
+++ node/app.js
@@ -12,14 +12,20 @@ const NUM = process.argv[2];
 const pipe_in = '/tmp/pipe_to_node_' + NUM;
 const pipe_out = '/tmp/pipe_from_node_' + NUM;
 
+// Check if file already exist
+if (!fs.existsSync(pipe_in) || !fs.existsSync(pipe_out)) {
+    console.error('Pipe does not exist. Exiting.');
+    process.exit(1);
+}
+
 // open pipe for reading
-const read = fs.createReadStream(pipe_in, { highWaterMark: 100000 });
-const write = fs.createWriteStream(pipe_out, { highWaterMark: 100000 });
+const read  = fs.createReadStream(  pipe_in,    { highWaterMark: 500000 });
+const write = fs.createWriteStream( pipe_out,   { highWaterMark: 500000 });
 
 // read from pipe and reverse the string and write to pipe
 read.on('data', (data) => {
     const input = data.toString();
-
+    
     const ret = esprima.parse(input, {
         range: true,
         comment: true
