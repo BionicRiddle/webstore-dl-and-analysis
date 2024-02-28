13fb8f2cba1f06a7a4fd42ab2accd661564e3fe2
Albin Karlsson <albkar@student.chalmers.se>
2024-02-28 16:31:25 +0100

    pipes work

--- node/app.js
+++ node/app.js
@@ -1,29 +1,39 @@
+const stream = require('node:stream'); 
+const fs = require('fs');
+const esprima = require('esprima');
+
 // Check args
 if (process.argv.length == 2 || isNaN(process.argv[2])) {
     console.error('Usage: node pipe.js <NUM>');
     process.exit(1);
 }
 
-const esprima = require('esprima');
-const fs = require('fs');
-
 // argument when running the script
 const NUM = process.argv[2];
 const pipe_in = '/tmp/pipe_to_node_' + NUM;
 const pipe_out = '/tmp/pipe_from_node_' + NUM;
+const BUFFER = 500000;
 
-// Check if file already exist
 if (!fs.existsSync(pipe_in) || !fs.existsSync(pipe_out)) {
     console.error('Pipe does not exist. Exiting.');
     process.exit(1);
 }
 
-// open pipe for reading
-const read  = fs.createReadStream(  pipe_in,    { highWaterMark: 500000 });
-const write = fs.createWriteStream( pipe_out,   { highWaterMark: 500000 });
+// Create a buffer 
+let data = '';
 
-// read from pipe and reverse the string and write to pipe
-read.on('data', (data) => {
+
+const read  = fs.createReadStream(pipe_in, { highWaterMark: BUFFER });
+
+read.on('data', (chunk) => {
+    console.log('Received data:', chunk.toString());
+    // Store the data in a buffer
+    data += chunk;
+});
+
+read.on('end', () => {
+    console.log('No more data to read');
+    // Now we do analysis on the data and return the result
     const input = data.toString();
     
     const ret = esprima.parse(input, {
@@ -31,6 +41,70 @@ read.on('data', (data) => {
         comment: true
     });
 
-    // reverse input and write to pipe
+    const write = fs.createWriteStream(pipe_out, { highWaterMark: BUFFER });
     write.write(JSON.stringify(ret, null, 2));
-})
+
+    // Close the write stream
+    write.end();
+
+    // Clear the buffer
+    data = '';
+
+});
+
+read.on('close', () => {
+    console.log('Named pipe closed');
+    keep_alive = false;
+    // Terminate this process
+});
+
+read.on('error', (err) => {
+    console.error('Error:', err);
+    // Terminate this process on error
+
+});
+
+
+
+// // Check if file already exist
+// if (!fs.existsSync(pipe_in) || !fs.existsSync(pipe_out)) {
+//     console.error('Pipe does not exist. Exiting.');
+//     process.exit(1);
+// }
+
+// while (true) {
+//     // open pipe for reading
+//     const read  = fs.createReadStream(  pipe_in,    { highWaterMark: 500000 });
+//     const write = fs.createWriteStream( pipe_out,   { highWaterMark: 500000 });
+
+//     // read from pipe and reverse the string and write to pipe
+//     read.on('data', (data) => {
+//         const input = data.toString();
+        
+//         const ret = esprima.parse(input, {
+//             range: true,
+//             comment: true
+//         });
+
+//         // reverse input and write to pipe
+//         write.write(JSON.stringify(ret, null, 2));
+//     })
+
+//     // close the pipe
+//     console.log('Closing the pipe.');
+// }
+
+// process.stdin.on('data', (data) => {
+//     const input = data.toString().trim(); // Trim to remove newline characters
+
+//     // if epmty line, do nothing
+//     if (input === '') {
+//         return;
+//     }
+
+//     // Perform actions based on the input
+//     if (input === 'exit' || input === 'quit' || input === 'q') {
+//         console.log('Exiting the application.');
+//         process.exit(); // Terminate the process
+//     }
+// });
--- pipes.py
+++ pipes.py
@@ -27,15 +27,22 @@ except:
 
 ## create named pipes
 
+pipe_from_node_fd = None
+
 try:
 
     while True:
         os.mkfifo(pipe_to_node)
         os.mkfifo(pipe_from_node)
 
-        ## open the pipes
+        input("Press Enter to continue...")
+
+        print("pipes created")
+
+        ## open the pipe
         pipe_to_node_fd = os.open(pipe_to_node, os.O_WRONLY)
-        pipe_from_node_fd = os.open(pipe_from_node, os.O_RDONLY)
+
+        print("pipes created")
 
         # read file "input_file" and send it to the Node.js process
         input_file = "node/app.js"
@@ -44,11 +51,15 @@ try:
             if file_content == '':
                 file_content = b'{}\n'
             os.write(pipe_to_node_fd, file_content)
-        os.close(pipe_to_node_fd)
+        
+
+        pipe_from_node_fd = os.open(pipe_from_node, os.O_RDONLY)
+        print("waiting for the node to finish")
 
         ## read from the pipe
         print(os.read(pipe_from_node_fd, 100))
-        os.close(pipe_from_node_fd)
+
+        print("done")
 
         remove_pipes()
 
@@ -56,6 +67,13 @@ except Exception as e:
     remove_pipes()
     raise
 except KeyboardInterrupt as e:
+    try:
+        os.close(pipe_from_node_fd)
+        os.close(pipe_to_node_fd)
+        print("closed!")
+    except:
+        print("pipe_from_node_fd already closed")
+        pass
     remove_pipes()
 
 
