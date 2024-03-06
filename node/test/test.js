3135cc214a7373f0314b47d1ffcc7798245f7c88
Albin Karlsson <albkar@student.chalmers.se>
2024-03-06 10:46:50 +0100

    esprima test

--- node/test/test.js
+++ node/test/test.js
@@ -12,15 +12,35 @@ let subdomain = "jsonplaceholder";
 let path = "posts";
 let url = schema + subdomain + "." + domain + "/" + path;
 
-fetch(url)
-.then(response => response.json())
-.then(data => {
-    for (let i = 0; i < data.length; i++) {
-        console.log(data[i].title);
-        // prepend to body
-        let p = document.createElement("p");
-        p.innerHTML = data[i].title;
-        document.body.prepend(p);
-    }
-  })
-  .catch(error => console.error(error));
+let test = 1;
+
+if (test == 0) {
+    url = "https://chalmers.se/";
+}
+
+switch (test) {
+    case 0:
+        url = "https://gp.se/";
+        break;
+    case 1:
+        url = "https://google.se/";
+        break;
+    default:
+        url = "https://kth.se/";
+        break;
+}
+
+if (test == 1) {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        for (let i = 0; i < data.length; i++) {
+            console.log(data[i].title);
+            // prepend to body
+            let p = document.createElement("p");
+            p.innerHTML = data[i].title;
+            document.body.prepend(p);
+        }
+      })
+      .catch(error => console.error(error));
+}
